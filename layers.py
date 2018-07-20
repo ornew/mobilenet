6edd1db79ede7e385638b3c623b2e7eb453c89cd
Arata <furukawa-arata@dmm.com>
2018-07-20 11:19:23 +0000

    upd

--- layers.py
+++ layers.py
@@ -12,46 +12,48 @@ def batch_normalization(
     training=training,
     *args, **kwargs)
 
+def add_bias(inputs, bias_name='biases'):
+  channel = get_channel(inputs)
+  b = tf.get_variable(bias_name, [channel])
+  x = tf.nn.bias_add(x, b)
+  return x
+
 def depthwise_conv2d(
     inputs,
     kernel_size=3,
     strides=1,
     dilation_rate=1,
     depth_multiplier=1,
+    use_bias=True,
     normalizer=None,
     activation=tf.nn.relu6,
-    use_bias=True,
     scope=None):
-  assert training is not None
   dilation_rate = get_pair(dilation_rate)
+  strides = get_pair(strides)
+  strides = [1,strides[0],strides[1],1]
   kernel_size = get_pair(kernel_size)
-  in_channel = get_channel(inputs)
+  channel = get_channel(inputs)
   with tf.variable_scope(scope, 'depthwise_conv2d', [inputs]):
     W = tf.get_variable('weights',
-        [kernel_size[0], kernel_size[1], in_channel, depth_multiplier],
-        regularizer=tf.no_regularizer,
-        initializer=kernel_initializer)
+        [kernel_size[0], kernel_size[1], channel, depth_multiplier])
     x = tf.nn.depthwise_conv2d(inputs, W, strides, 'SAME', rate=dilation_rate)
-    if use_bias:
-      b = tf.get_variable('biases',
-          [in_channel * depth_multiplier],
-          initializer=bias_initializer)
-      x = tf.nn.bias_add(x, b)
+    x = add_bias(x) if use_bias else x
     x = normalizer(x) if normalizer else x
     x = activation(x) if activation else x
     return x
 
-def pointwise_conv2d(inputs, filters, training, scope=None):
+def pointwise_conv2d(
+    inputs,
+    filters,
+    use_bias=True,
+    normalizer=None,
+    activation=tf.nn.relu6,
+    scope=None):
+  channel = get_channel(inputs)
   with tf.variable_scope(scope, 'pointwise_conv2d', [inputs]):
-    W = tf.get_variable('weight',
-        [1, 1, depth_output_channel, filters],
-        initializer=kernel_initializer)
+    W = tf.get_variable('weight', [1, 1, channel, filters])
     x = tf.nn.conv2d(x, W, [1, 1, 1, 1], 'SAME')
-    if use_bias:
-      b = tf.get_variable('biases',
-          [in_channel * depth_multiplier],
-          initializer=bias_initializer)
-      x = tf.nn.bias_add(x, b)
+    x = add_bias(x) if use_bias else x
     x = normalizer(x) if normalizer else x
     x = activation(x) if activation else x
     return x
@@ -63,33 +65,49 @@ def separable_conv2d(
     strides=1,
     dilation_rate=1,
     depth_multiplier=1,
-    training=None,
-    depthwise_kernel_initializer=tf.glorot_uniform_initializer(),
-    depthwise_bias_initializer=tf.zeros_initializer(),
-    pointwise_kernel_initializer=tf.glorot_uniform_initializer(),
-    pointwise_bias_initializer=tf.zeros_initializer(),
+    use_bias=True,
+    normalizer=None,
+    activation=tf.nn.relu6,
     scope=None):
-  
-  depth_output_channel = int(in_channel * depth_multiplier)
+  x = inputs
   with tf.variable_scope(scope, 'separable_conv2d'):
-    x = depthwise_conv2d(x)
-    x = pointwise_conv2d(x)
+    x = depthwise_conv2d(x,  kernel_size, strides, dilation_rate,
+        depth_multiplier, use_bias, normalizer, activation)
+    x = pointwise_conv2d(x, filters, use_bias, normalizer, activation)
     return x
 
-def expanded_conv2d(inputs, filters, stride, expantion_rate, is_training):
-  """Expanded Separable Convolution2d Layer"""
+def expanded_conv2d(
+    inputs,
+    filters,
+    kernel_size=3,
+    strides=1,
+    dilation_rate=1,
+    depth_multiplier=1,
+    expantion_rate=6,
+    use_bias=True,
+    normalizer=None,
+    activation=tf.nn.relu6,
+    residual=True):
+  """Expanded Convolution-2d Layer"""
+  x = inputs
   with tf.variable_scope(None, 'expanded_conv2d', [inputs]):
-    x = inputs
     assert expantion_rate >= 1
-    channel = _get_channel(inputs)
-    num_outputs = int(expantion_rate * channel)
-    if expantion_rate > 1:
-        x = _expansion_conv2d_layer(x, expantion_rate, is_training, activation=tf.nn.relu6)
-    x = _depthwise_conv2d_layer(x, stride,  is_training, activation=tf.nn.relu6)
-    x = _pointwise_conv2d_layer(x, filters, is_training, activation=None)
+    channel = get_channel(inputs)
+    expand_filters = int(expantion_rate * channel)
+    x = pointwise_conv2d(x, expand_filters,
+                         normalizer=normalizer, activation=activation,
+                         scope='expand') if expantion_rate > 1 else x
+    x = depthwise_conv2d(x, kernel_size, strides, dilation_rate,
+                         depth_multiplier, use_bias=use_bias, normalizer=normalizer,
+                         activation=activation, scope='depthwise')
+    x = pointwise_conv2d(x, filters, use_bias=use_bias, normalizer=normalizer,
+                         activation=None, scope='project')
 
-    if stride == 1 and _get_channel(inputs) == _get_channel(x):
-        x = tf.add(inputs, x) # residual connection.
+    # residual connection.
+    if callable(residual):
+      x = residual(inputs, x)
+    elif residual and stride == 1 and get_channel(inputs) == get_channel(x):
+      x = tf.nn.add(inputs, x)
     return x
 
 def global_average_pooling(inputs, scope=None):
--- nets/mobilenet_v2.py
+++ nets/mobilenet_v2.py
@@ -11,130 +11,66 @@ This program was implemented with reference to the following papers.
 > arXiv preprint arXiv:1801.04381 (2018).
 """
 
-from collections import namedtuple
 import tensorflow as tf
 
+from layers import conv2d, expanded_conv2d
+
 Conv = Convolution = namedtuple(
-    'Convolution',
-    ['kernel_size', 'filters', 'strides'])
+  'Convolution',
+  ['kernel_size', 'filters', 'strides'])
 ExpSepConv = ExpandedSeparableConvolution = namedtuple(
-    'ExpandedSeparableConvolution',
-    ['expantion_rate', 'filters', 'strides'])
+  'ExpandedSeparableConvolution',
+  ['expantion_rate', 'filters', 'strides'])
 
 MOBILENET_V2_LAYERS = [
-    Conv      (3,   32, 2),
-    ExpSepConv(1,   16, 1),
-    ExpSepConv(6,   24, 2),
-    ExpSepConv(6,   24, 1),
-    ExpSepConv(6,   32, 2),
-    ExpSepConv(6,   32, 1),
-    ExpSepConv(6,   32, 1),
-    ExpSepConv(6,   64, 2),
-    ExpSepConv(6,   64, 1),
-    ExpSepConv(6,   64, 1),
-    ExpSepConv(6,   64, 1),
-    ExpSepConv(6,   96, 1),
-    ExpSepConv(6,   96, 1),
-    ExpSepConv(6,   96, 1),
-    ExpSepConv(6,  160, 2),
-    ExpSepConv(6,  160, 1),
-    ExpSepConv(6,  160, 1),
-    ExpSepConv(6,  320, 1),
-    Conv      (1, 1280, 1),
+  Conv      (3,   32, 2),
+  ExpSepConv(1,   16, 1),
+  ExpSepConv(6,   24, 2),
+  ExpSepConv(6,   24, 1),
+  ExpSepConv(6,   32, 2),
+  ExpSepConv(6,   32, 1),
+  ExpSepConv(6,   32, 1),
+  ExpSepConv(6,   64, 2),
+  ExpSepConv(6,   64, 1),
+  ExpSepConv(6,   64, 1),
+  ExpSepConv(6,   64, 1),
+  ExpSepConv(6,   96, 1),
+  ExpSepConv(6,   96, 1),
+  ExpSepConv(6,   96, 1),
+  ExpSepConv(6,  160, 2),
+  ExpSepConv(6,  160, 1),
+  ExpSepConv(6,  160, 1),
+  ExpSepConv(6,  320, 1),
+  Conv      (1, 1280, 1),
 ]
 
-def _get_channel(tensor):
-    channel_axis = -1 # NHWC is -1, NCHW is 1
-    channel = tensor.get_shape().as_list()[channel_axis]
-    assert channel is not None
-    return channel
-
 def _multiple(value, multipler, divisor):
-    assert multipler > 0
-    assert divisor > 0
-    return max(divisor, (value * multipler + divisor / 2) // divisor * divisor)
+  assert multipler > 0
+  assert divisor > 0
+  return max(divisor, (value * multipler + divisor / 2) // divisor * divisor)
 
-def _expansion_conv2d_layer(inputs, expantion_rate, is_training, activation=None):
-    channel = _get_channel(inputs)
-    num_outputs = int(expantion_rate * channel)
-    with tf.variable_scope(None, 'expansion_conv2d_layer', [inputs]):
-        x = tf.layers.conv2d(inputs, num_outputs, 1, 1, 'SAME', use_bias=False)
-        x = tf.layers.batch_normalization(x, training=is_training)
-        if activation:
-            x = activation(x)
-        return x
+def mobilenet_v2(inputs, multiplier=None, scope=None):
+  with tf.variable_scope(scope, 'mobilenet_v2', [inputs]):
+    conv2d()
 
-def _depthwise_conv2d_layer(inputs, stride, is_training, depth_multiplier=1, activation=None):
-    channel = _get_channel(inputs)
-    dtype = inputs.dtype.base_dtype
-    shape = [3, 3, channel, depth_multiplier]
-    strides = [1, stride, stride, 1]
-    with tf.variable_scope(None, 'depthwise_conv2d_layer', [inputs]):
-        kernel = tf.get_variable('kernel', shape=shape, dtype=dtype)
-        x = tf.nn.depthwise_conv2d(
-            inputs, kernel, strides, 'SAME', data_format='NHWC')
-        x = tf.layers.batch_normalization(x, training=is_training)
-        if activation:
-            x = activation(x)
-        return x
+def mobilenet_v2(inputs, multiplier=None, scope=None):
+  with tf.variable_scope(scope, 'mobilenet_v2', [inputs]):
+    x = inputs
+    for i, l in enumerate(MOBILENET_V2_LAYERS):
+      with tf.variable_scope('hidden_layer_{}'.format(i)):
+        num_outputs = _multiple(l.filters, multiplier, 8) \
+          if multiplier is not None else l.filters
 
-def _pointwise_conv2d_layer(inputs, filters, is_training, activation=None):
-    with tf.variable_scope(None, 'pointwise_conv2d_layer', [inputs]):
-        x = tf.layers.conv2d(inputs, filters, 1, 1, 'SAME', use_bias=False)
-        x = tf.layers.batch_normalization(x, training=is_training)
-        if activation:
-            x = activation(x)
-        return x
+        if isinstance(l, Convolution):
+          x = tf.layers.conv2d(
+              x, num_outputs, l.kernel_size, l.strides, 'SAME', use_bias=False)
+          x = tf.layers.batch_normalization(x, training=is_training)
+          x = tf.nn.relu6(x)
 
-def expanded_separable_convolution2d(inputs, filters, stride, expantion_rate, is_training):
-    """Expanded Separable Convolution2d Layer
-    """
-    with tf.variable_scope(None, 'expanded_separable_convolution2d_layer', [inputs]):
-        x = inputs
-        assert expantion_rate >= 1
-        if expantion_rate > 1:
-            x = _expansion_conv2d_layer(x, expantion_rate, is_training, activation=tf.nn.relu6)
-        x = _depthwise_conv2d_layer(x, stride,  is_training, activation=tf.nn.relu6)
-        x = _pointwise_conv2d_layer(x, filters, is_training, activation=None)
+        elif isinstance(l, ExpandedSeparableConvolution):
+          x = expanded_separable_convolution2d(
+              x, num_outputs, l.strides, l.expantion_rate, is_training)
 
-        if stride == 1 and _get_channel(inputs) == _get_channel(x):
-            x = tf.add(inputs, x) # residual connection.
-        return x
-
-def mobilenet(inputs, is_training, multiplier=None, scope=None):
-    """Embedding Feature by MobileNet v2
-    """
-    with tf.variable_scope(scope, 'mobilenet_v2', [inputs]):
-        x = inputs
-        for i, l in enumerate(MOBILENET_V2_LAYERS):
-            with tf.variable_scope('hidden_layer_{}'.format(i)):
-                num_outputs = _multiple(l.filters, multiplier, 8) \
-                    if multiplier is not None else l.filters
-
-                if isinstance(l, Convolution):
-                    x = tf.layers.conv2d(
-                        x, num_outputs, l.kernel_size, l.strides, 'SAME', use_bias=False)
-                    x = tf.layers.batch_normalization(x, training=is_training)
-                    x = tf.nn.relu6(x)
-
-                elif isinstance(l, ExpandedSeparableConvolution):
-                    x = expanded_separable_convolution2d(
-                        x, num_outputs, l.strides, l.expantion_rate, is_training)
-
-            tf.logging.debug('mobilenet_v2.hidden_layer.{:02}: output_shape={}'.format(
-                i, x.get_shape().as_list()))
-        return x
-
-def classify(
-        inputs, num_classes, is_training, multiplier=None,
-        dropout_rate=0.8, scope=None):
-    """MobileNet v2 Classification
-    """
-    with tf.variable_scope(scope, 'mobilenet_v2_classify', [inputs]):
-        x = mobilenet(inputs, is_training, multiplier=multiplier)
-        x = tf.reduce_mean(x, [1, 2], keepdims=True, name='global_average_pooling')
-        x = tf.layers.dropout(x, rate=dropout_rate, training=is_training)
-        x = tf.layers.conv2d(x, num_classes, 1, name='readout')
-        x = tf.squeeze(x, [1, 2])
-        assert x.get_shape().as_list() == [None, num_classes]
-        return x
+      tf.logging.debug('mobilenet_v2.hidden_layer.{:02}: output_shape={}'.format(
+          i, x.get_shape().as_list()))
+  return x
